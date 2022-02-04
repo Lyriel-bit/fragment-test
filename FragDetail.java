d78fa6dfcc2832f5ca3c714bacfd9425e7f0413c
Lyriel-bit <99033040+Lyriel-bit@users.noreply.github.com>
2022-02-04 17:05:29 +0100

    Add files via upload

---
+++ FragDetail.java
@@ -0,0 +1,28 @@
+package it.univaq.fragmentapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragDetail extends Fragment {
+
+
+    public FragDetail() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag_detail, container, false);
+    }
+}
